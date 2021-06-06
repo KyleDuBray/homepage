245521e478b463025af2c7c3b1844d2a86dc54d3
DuBrowski <krdubray@comcast.net>
2021-06-06 16:23:35 -0500

    improved weather icon mapping to include night icons

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,8 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/weather.css";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchLocation } from "../actions";
-import moment from "moment";
 
 const weatherStyles = {
   sunny: "sunny-outline",
@@ -18,44 +17,73 @@ const weatherStyles = {
 const Weather = () => {
   const dispatch = useDispatch();
   const weather = useSelector((state) => state.weather.condition);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     dispatch(fetchLocation());
   }, []);
+
+  useEffect(() => {
+    if (weather) {
+      console.log(weather);
+      setLoading(false);
+    } else setLoading(true);
+  }, [weather]);
 
   const renderWeatherUnits = () => {
     return !weather ? null : <>&#176;F</>;
   };
 
-  // Return appropriate icon for weather based on condition
-  // TODO: make function that calculates dark or light outside
-  // and optionally return night icons
   const renderWeatherIcon = () => {
+    if (loading) return null;
     const currentWeather = weather.weather[0]?.description;
-    if (!currentWeather) return null;
 
-    if (currentWeather.includes("storm")) {
-      return weatherStyles.storm;
-    } else if (
-      currentWeather.includes("rain") ||
-      currentWeather.includes("drizzle")
-    ) {
-      return weatherStyles.rainy;
-    } else if (currentWeather.includes("snow")) {
-      return weatherStyles.snow;
-    } else if (currentWeather.includes("clear")) {
-      return weatherStyles.sunny;
-    } else if (currentWeather.includes("cloud")) {
-      if (currentWeather.includes("scattered")) {
-        return weatherStyles.cloudy;
-      } else return weatherStyles.sunnyPartly;
-    } else return weatherStyles.sunny;
+    if (isDayTime()) {
+      if (currentWeather.includes("storm")) {
+        return weatherStyles.storm;
+      } else if (
+        currentWeather.includes("rain") ||
+        currentWeather.includes("drizzle")
+      ) {
+        return weatherStyles.rainy;
+      } else if (currentWeather.includes("snow")) {
+        return weatherStyles.snow;
+      } else if (currentWeather.includes("clear")) {
+        return weatherStyles.sunny;
+      } else if (currentWeather.includes("cloud")) {
+        if (currentWeather.includes("scattered")) {
+          return weatherStyles.cloudy;
+        } else return weatherStyles.sunnyPartly;
+      } else return weatherStyles.cloudy;
+    } else {
+      if (currentWeather.includes("storm")) {
+        return weatherStyles.storm;
+      } else if (
+        currentWeather.includes("rain") ||
+        currentWeather.includes("drizzle")
+      ) {
+        return weatherStyles.rainy;
+      } else if (currentWeather.includes("clear")) {
+        return weatherStyles.night;
+      } else return weatherStyles.nightCloudy;
+    }
   };
 
-  console.log(weather.weather[0].description);
+  const isDayTime = () => {
+    if (loading) return null;
+    const sys = weather?.sys;
+    const sunriseTime = sys.sunrise * 1000;
+    const sunsetTime = sys.sunset * 1000;
+    const curTime = Date.now();
+    if (curTime < sunriseTime || curTime > sunsetTime) {
+      return false;
+    } else return true;
+  };
 
-  renderWeatherIcon();
-  return (
+  return loading ? (
+    "Loading..."
+  ) : (
     <div className="weather-container">
       <div className="weather-city">
         <h4>{!weather ? null : weather.name}</h4>
